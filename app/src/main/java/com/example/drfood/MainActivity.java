9263ecce233f6eb7c1c5f3471027e75b3fb96ad3
han <taxol1203@pusan.ac.kr>
2019-08-14 13:39:48 +0900

    병신주연을 넣었습니다

--- app/src/main/java/com/example/drfood/MainActivity.java
+++ app/src/main/java/com/example/drfood/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //안녕하세요 저는 병신주연입낟.
     }
 }
