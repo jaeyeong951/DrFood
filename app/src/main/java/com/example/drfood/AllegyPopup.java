9552d5b837a9e4198ca27bb058a6333c6aeeaa8d
bch96 <bch960414@gmail.com>
2019-08-28 18:27:33 +0900

    DB Intent to product_page

---
+++ app/src/main/java/com/example/drfood/AllegyPopup.java
@@ -0,0 +1,76 @@
+package com.example.drfood;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class AllegyPopup extends Activity {
+
+    TextView txtText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //타이틀바 없애기
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.allegy_popup_activity);
+
+
+        // 빈 데이터 리스트 생성.
+        final ArrayList<String> items = new ArrayList<String>() ;
+
+        items.add("No");
+
+        // ArrayAdapter 생성. 아이템 View를 선택(multiple choice)가능하도록 만듦.
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, items) ;
+
+        // listview 생성 및 adapter 지정.
+        final ListView listview = (ListView) findViewById(R.id.Allegy_List) ;
+        listview.setAdapter(adapter) ;
+
+        /*
+        //UI 객체생성
+        txtText = (TextView)findViewById(R.id.txtText);
+
+        //데이터 가져오기
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("data");
+        txtText.setText(data);
+        */
+    }
+
+    //확인 버튼 클릭
+    public void mOnClose(View v){
+        //데이터 전달하기
+        Intent intent = new Intent();
+        intent.putExtra("result", "Close Popup");
+        setResult(RESULT_OK, intent);
+
+        //액티비티(팝업) 닫기
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //바깥레이어 클릭시 안닫히게
+        if(event.getAction()==MotionEvent.ACTION_OUTSIDE){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        //안드로이드 백버튼 막기
+        return;
+    }
+}
